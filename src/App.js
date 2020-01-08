9ffae9ae28a90efc2f0e9464d1336db4ee072a6f
neuronet.io <neuronet.io@gmail.com>
2020-01-08 16:20:41 +0100

    initial working example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from "react";
+import GSTC from "gantt-schedule-timeline-calendar";
+import "gantt-schedule-timeline-calendar/dist/style.css";
+
+function GSTCWrapper() {
+  const config = {
+    height: 300,
+    list: {
+      rows: {
+        "1": {
+          id: "1",
+          label: "Row 1"
+        },
+        "2": {
+          id: "2",
+          label: "Row 2"
+        },
+        "3": {
+          id: "3",
+          label: "Row 3"
+        },
+        "4": {
+          id: "4",
+          label: "Row 4"
+        }
+      },
+      columns: {
+        data: {
+          id: {
+            id: "id",
+            data: "id",
+            width: 50,
+            header: {
+              content: "ID"
+            }
+          },
+          label: {
+            id: "label",
+            data: "label",
+            width: 200,
+            header: {
+              content: "Label"
+            }
+          }
+        }
+      }
+    },
+    chart: {
+      items: {
+        "1": {
+          id: "1",
+          rowId: "1",
+          label: "Item 1",
+          time: {
+            start: new Date().getTime(),
+            end: new Date().getTime() + 24 * 60 * 60 * 1000
+          }
+        },
+        "2": {
+          id: "2",
+          rowId: "2",
+          label: "Item 2",
+          time: {
+            start: new Date().getTime() + 4 * 24 * 60 * 60 * 1000,
+            end: new Date().getTime() + 5 * 24 * 60 * 60 * 1000
+          }
+        },
+        "3": {
+          id: "3",
+          rowId: "2",
+          label: "Item 3",
+          time: {
+            start: new Date().getTime() + 6 * 24 * 60 * 60 * 1000,
+            end: new Date().getTime() + 7 * 24 * 60 * 60 * 1000
+          }
+        },
+        "4": {
+          id: "4",
+          rowId: "3",
+          label: "Item 4",
+          time: {
+            start: new Date().getTime() + 10 * 24 * 60 * 60 * 1000,
+            end: new Date().getTime() + 12 * 24 * 60 * 60 * 1000
+          }
+        },
+        "5": {
+          id: "5",
+          rowId: "4",
+          label: "Item 5",
+          time: {
+            start: new Date().getTime() + 12 * 24 * 60 * 60 * 1000,
+            end: new Date().getTime() + 14 * 24 * 60 * 60 * 1000
+          }
+        }
+      }
+    }
+  };
+  let state = GSTC.api.stateFromConfig(config);
+  const callback = useCallback(
+    node => {
+      if (node) {
+        console.log("mounting");
+        GSTC({
+          element: node,
+          state
+        });
+      }
+    },
+    [state]
+  );
+
+  return <div ref={callback} />;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GSTCWrapper />
     </div>
   );
 }
