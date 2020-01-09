3ba3f4c71d7f8c636d53d8256a6e6bf92e0c392f
neuronet.io <neuronet.io@gmail.com>
2020-01-09 11:41:50 +0100

    gstc update

--- src/GSTC.js
+++ src/GSTC.js
@@ -1,14 +1,18 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import GSTC from "gantt-schedule-timeline-calendar";
 import "gantt-schedule-timeline-calendar/dist/style.css";
 
+let gstc;
 export default function GSTCWrapper(props) {
+  // @ts-ignore
   let state = GSTC.api.stateFromConfig(props.config);
   props.onState(state);
+
   const callback = useCallback(
     node => {
       if (node) {
-        GSTC({
+        // @ts-ignore
+        gstc = GSTC({
           element: node,
           state
         });
@@ -16,6 +20,14 @@ export default function GSTCWrapper(props) {
     },
     [state]
   );
+
+  useEffect(() => {
+    return () => {
+      if (gstc) {
+        gstc.app.destroy();
+      }
+    };
+  });
 
   return <div ref={callback} />;
 }
